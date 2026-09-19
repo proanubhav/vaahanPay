ae3cf49f41065cdc62ab93ea2d9672a5287c0268
Anubhav Gupta <proanubhav@gmail.com>
2026-09-19 15:54:36 +0530

    ui fix

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,36 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { afterNextRender, Component, DestroyRef, ElementRef, inject, signal, viewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
+  host: {
+    '(window:scroll)': 'onScroll()',
+    '[style.--header-height]': 'headerHeight() + "px"',
+  },
 })
 export class HomeComponent {
-  
+  private readonly document = inject(DOCUMENT);
+  private readonly destroyRef = inject(DestroyRef);
+  private readonly header = viewChild.required<ElementRef<HTMLElement>>('header');
+  protected readonly headerHeight = signal(0);
+  protected readonly isScrolled = signal(false);
+
+  constructor() {
+    afterNextRender(() => {
+      const element = this.header().nativeElement;
+      const updateHeight = () => this.headerHeight.set(element.getBoundingClientRect().height);
+      updateHeight();
+      this.onScroll();
+
+      const observer = new ResizeObserver(updateHeight);
+      observer.observe(element);
+      this.destroyRef.onDestroy(() => observer.disconnect());
+    });
+  }
+
+  protected onScroll(): void {
+    this.isScrolled.set((this.document.defaultView?.scrollY ?? 0) > 0);
+  }
 }
